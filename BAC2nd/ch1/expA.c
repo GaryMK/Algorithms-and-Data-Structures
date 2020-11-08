2055075732ab86f06efc0a79a68d784867e5aa46
GaryMK <49356298+GaryMK@users.noreply.github.com>
2020-11-08 10:34:57 +0800

    expA、B

---
+++ BAC2nd/ch1/expA.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    // A1
+    printf("%d\n", sizeof(int));
+    printf("%d\n", 11111*11111);
+    printf("%d\n", 111111*111111);
+    printf("%d\n", 111111111*111111111);
+    printf("\n");
+    // A2
+    printf("%d\n", sizeof(float));
+    printf("%f\n", 11111.0*11111.0);
+    printf("%f\n", 111111.0*111111.0);
+    printf("%f\n", 111111111.0*111111111.0);
+    printf("\n");
+    // A3
+    printf("%f", sqrt(-10));
+    printf("\n");
+    // A4
+    printf("%f\t%f\n", 1.0/1.0, 0.0/0.0);
+    printf("\n");
+    // A5
+    printf("%f\n", 1/0);
+    printf("\n");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Algorithms
 Algorithmic learning notes
 
+[cppreference.com](https://en.cppreference.com/w/)
+
 ## BAC2nd
 
-《算法竞赛入门经典第二版》学习笔记，记录了本书涉及的相关例题的代码
+《算法竞赛入门经典第二版》学习笔记，记录了本书涉及的相关例题
