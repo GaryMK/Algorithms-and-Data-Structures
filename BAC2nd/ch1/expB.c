877df3bd2471e603680ca55d21718071f99f0808
GaryMK <49356298+GaryMK@users.noreply.github.com>
2020-11-09 08:36:22 +0800

    update expB

--- BAC2nd/ch1/expB.c
+++ BAC2nd/ch1/expB.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    //int a,b;
-
-    //scanf("%d%d", &a, &b);
-    //printf("%d %d\n", a, b);
+    int a,b;
+    scanf("%d%d", &a, &b);
+    printf("%d %d\n", a, b);
     printf("%%d\\n");
     return 0;
 }
